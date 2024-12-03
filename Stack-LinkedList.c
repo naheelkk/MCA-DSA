bc09b5dd5b3fe3abbd87bc9c105c25cd7f02bca1
Naheel <96335640+naheelkk@users.noreply.github.com>
2024-12-04 00:25:03 +0530

    Update Stack-LinkedList.c

--- Stack-LinkedList.c
+++ Stack-LinkedList.c
@@ -10,24 +10,24 @@ struct Node *header = NULL;
 struct Node *CreateNode(int data)
 {
     struct Node *newnode;
-    newnode = (struct Node *) malloc(sizeof(struct Node));
-    newnode ->data = data;
-    newnode -> link = NULL;
-    return(newnode);
+    newnode = (struct Node *)malloc(sizeof(struct Node));
+    newnode->data = data;
+    newnode->link = NULL;
+    return (newnode);
 };
 
 void traverse()
 {
     struct Node *ptr = header;
-    if(ptr == NULL)
+    if (ptr == NULL)
         printf("\nEmpty Stack\n");
     else
     {
         printf("\n");
-        while(ptr != NULL)
+        while (ptr != NULL)
         {
-            printf("%d\t",ptr->data);
-            ptr = ptr -> link;
+            printf("%d\t", ptr->data);
+            ptr = ptr->link;
         }
     }
     printf("\n");
@@ -37,14 +37,14 @@ void push(int data)
 {
     struct Node *newnode;
     newnode = CreateNode(data);
-    newnode -> link = header;
+    newnode->link = header;
     header = newnode;
 }
 
 void pop()
 {
     struct Node *ptr = header;
-    if(ptr == NULL)
+    if (ptr == NULL)
     {
         printf("\nEmpty Stack\n");
         return;
@@ -52,24 +52,24 @@ void pop()
     else
     {
         printf("\nPopped item from top\n");
-        header = ptr -> link;
+        header = ptr->link;
         free(ptr);
     }
 }
 
 int main()
 {
-    int choice,data;
-    while(choice != 4)
+    int choice, data;
+    while (choice != 4)
     {
         printf("\n1.Push\n2.Pop\n3.Traverse\n4.Exit\n");
         printf("Enter choice ");
-        scanf("%d",&choice);
-        switch(choice)
+        scanf("%d", &choice);
+        switch (choice)
         {
         case 1:
             printf("Enter Data ");
-            scanf("%d",&data);
+            scanf("%d", &data);
             push(data);
             break;
         case 2:
@@ -85,7 +85,6 @@ int main()
             printf("\nInvalid Choice");
             break;
         }
-
     }
-    return(0);
+    return (0);
 }
