253215f27106c270088810888fab4008c0782148
Naheel <96335640+naheelkk@users.noreply.github.com>
2024-12-04 00:23:13 +0530

    Update Stack-LinkedList.c

--- Stack-LinkedList.c
+++ Stack-LinkedList.c
@@ -1,76 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node{
+struct Node
+{
     int data;
     struct Node *link;
 };
-
 struct Node *header = NULL;
 struct Node *CreateNode(int data)
 {
-struct Node *newnode;
-newnode = (struct Node *) malloc(sizeof(struct Node));
-newnode -> data = data;
-newnode -> link = NULL;
-return(newnode);
+    struct Node *newnode;
+    newnode = (struct Node *) malloc(sizeof(struct Node));
+    newnode ->data = data;
+    newnode -> link = NULL;
+    return(newnode);
 };
+
+void traverse()
+{
+    struct Node *ptr = header;
+    if(ptr == NULL)
+        printf("\nEmpty Stack\n");
+    else
+    {
+        printf("\n");
+        while(ptr != NULL)
+        {
+            printf("%d\t",ptr->data);
+            ptr = ptr -> link;
+        }
+    }
+    printf("\n");
+}
+
 void push(int data)
 {
     struct Node *newnode;
-    struct Node *ptr = header;
     newnode = CreateNode(data);
-    if(header == NULL)
-    {
-        header = newnode;
-    }
-    else
-    {
-        while(ptr -> link != NULL)
-        {
-            ptr = ptr -> link;
-        }
-        ptr->link = newnode;
-    }
-
+    newnode -> link = header;
+    header = newnode;
 }
 
 void pop()
 {
     struct Node *ptr = header;
-    if(header == NULL)
+    if(ptr == NULL)
     {
-        printf("Empty List");
+        printf("\nEmpty Stack\n");
         return;
     }
     else
     {
+        printf("\nPopped item from top\n");
         header = ptr -> link;
         free(ptr);
     }
-
 }
-
-void traverse()
-{
-    struct Node *ptr = header;
-    while(ptr != NULL)
-    {
-        printf("%d\t",ptr->data);
-        ptr = ptr->link;
-    }
-}
-
 
 int main()
 {
-    printf("Hello world!\n");
-    push(10);
-    push(20);
-    push(40);
-    traverse();
-    pop();
-    printf("\n");
-    traverse();
-    return 0;
+    int choice,data;
+    while(choice != 4)
+    {
+        printf("\n1.Push\n2.Pop\n3.Traverse\n4.Exit\n");
+        printf("Enter choice ");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+        case 1:
+            printf("Enter Data ");
+            scanf("%d",&data);
+            push(data);
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            traverse();
+            break;
+        case 4:
+            printf("Exiting");
+            return;
+        default:
+            printf("\nInvalid Choice");
+            break;
+        }
+
+    }
+    return(0);
 }
